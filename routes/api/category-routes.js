93559f95b4b7bb1c2183bcab8bb7c43db1eee490
Carolina Haggerty <work@Carolinas-Mac.local>
2023-05-03 22:30:35 -0700

    updates

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,24 +5,68 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+      // associated Products
+      include: [{model:Product}]
+    }).then(categoryData =>{
+      res.status(200).json(categoryData);
+    }).catch(err =>{
+    res.status(500).json(err);
+  })
 });
-
+// find one category by its `id` value
+  //include its associated Products
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findByPk(req.params.id,{
+      include:[{model:Product}]
+    }).then(categoryData =>{
+      if(!categoryData){
+        return res.json(404).json({message:'No category found with this id'});
+      }
+      res.status(200).json(categoryData);
+    }).catch(err =>{
+    res.status(500).json(err);
+  })  
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+// create a new category
+router.post('/',  (req, res) => {
+  Category.create(req.body)
+  .then(categoryData =>{
+    res.status(200).json(categoryData);
+  }).catch(err =>{
+    res.status(500).json(err);
+  })
 });
 
+// update a category by its `id` value
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  Category.update(req.body,{
+      where:{id:req.params.id}
+    }).then(categoryData =>{
+      if(!categoryData[0]){
+        return res.status(404).json({message:"No category with that id has been updated"})
+      }
+      res.status(200).json(categoryData);
+    }).catch(err=>{
+    res.status(500).json(err);
+  })
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+      where:{
+        id:req.params.id, 
+      }
+    }).then(categoryData =>{
+      if(!categoryData){
+        return res.status(404).json({message:'No category with that id to delete'})
+      }
+      res.status(200).json(categoryData)
+    }).catch(err=>{
+    res.status(500).json(err);
+  })
 });
 
 module.exports = router;
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,7 +7,17 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include:[{model:Category, model:Tag}]
+  }).then(productData =>{
+    if(productData.length === 0){
+      res.status(404).json({message:'No products in this DataBase'})
+    }
+    res.status(200).json(productData);
+  }).catch(err=>{
+    res.status(500).json(err);
 });
+})
 
 // get one product
 router.get('/:id', (req, res) => {
